5f5be860733f73d1829abee827f54fc443bda800
Kyrylo Cherdakliiev <yungkeryaa@gmail.com>
2025-11-23 19:28:02 +0100

    Implemented Person class and conversion to list of Person's objects

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_data: list) -> list:
+    # new_person = None
+    Person.people = {}
+    for person_item in people_data:
+        new_person = Person(person_item["name"],
+                            person_item["age"])
+        if "wife" in person_item and person_item["wife"] is not None:
+            # new_person.wife = Person.people[person_item["wife"]]
+            new_person.wife = person_item["wife"]
+        if "husband" in person_item and person_item["husband"] is not None:
+            new_person.husband = person_item["husband"]
+
+    for person_item in Person.people.values():
+        if hasattr(person_item, "wife"):
+            person_item.wife = Person.people[person_item.wife]
+        if hasattr(person_item, "husband"):
+            person_item.husband = Person.people[person_item.husband]
+
+    return [person for person in Person.people.values()]
